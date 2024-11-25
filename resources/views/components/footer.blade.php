b93d43e5e529a5c5d3c9859be82e4f945edfa646
jei3m <virozzegt@gmail.com>
2024-11-25 14:21:52 +0800

    reduced text in footer component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,8 +2,6 @@
     <div class="container mx-auto px-4 py-8">
         <div class="text-center text-gray-300">
             <p>&copy; {{ date('Y') }} Your E-Commerce Store. All rights reserved.</p>
-            <p>Design: Justin Miguel</p>
-
         </div>
     </div>
 </footer>
