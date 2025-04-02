a63f55c46c729fdb7a5c40e848f108ba65afdd73
jei3m <virozzegt@gmail.com>
2025-04-02 21:13:07 +0800

    del: remove unused function

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -9,11 +9,6 @@ class CartItem extends Model
 {
     protected $fillable = ['user_id', 'product_id', 'quantity'];
 
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
